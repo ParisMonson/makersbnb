08b0b91b66964f5465fc470e7a83fe961856346d
Paris Monson <parismonson@yahoo.com>
2022-07-19 16:04:48 +0100

    Added database details

---
+++ spec/seeds/makers_bnb_new.sql
@@ -0,0 +1,60 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+--ASSUMPTIONS:
+-- users are hosts and guests
+-- a host can also be a guest
+-- a guest can also be a host
+-- one host can have many properties
+-- one guest can have many properties
+-- one property can have only one host
+-- one property can have many guests but one guest at any given time
+
+
+DROP TABLE IF EXISTS "public"."users" CASCADE;
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id uuid DEFAULT uuid_generate_v4 (),
+  first_name text NOT NULL,
+  last_name text NOT NULL,
+  email citext NOT NULL UNIQUE,
+  password text NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+DROP TABLE IF EXISTS "public"."spaces" CASCADE;
+
+CREATE TABLE IF NOT EXISTS spaces (
+  space_id uuid DEFAULT uuid_generate_v4 (),
+  title text NOT NULL,
+  description varchar (500),
+  address text NOT NULL,
+  price_per_night money NOT NULL,
+  available_from date,
+  available_to date,
+  host_id uuid NOT NULL,
+  PRIMARY KEY (space_id),
+  CONSTRAINT fk_host 
+    FOREIGN KEY (host_id) REFERENCES users(user_id)
+);
+
+DROP TABLE IF EXISTS "public"."reservations" CASCADE;
+
+CREATE TABLE IF NOT EXISTS reservations (
+  id uuid DEFAULT uuid_generate_v4 (),
+  host_id uuid NOT NULL,
+  guest_id uuid NOT NULL,
+  space_id uuid NOT NULL,
+  start_date date NOT NULL,
+  end_date date NOT NULL,
+  number_night int NOT NULL,
+  confirmed boolean,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_host 
+    FOREIGN KEY (host_id) REFERENCES users(user_id),
+  CONSTRAINT fk_guest 
+    FOREIGN KEY (guest_id) REFERENCES users(user_id),
+  CONSTRAINT fk_space 
+    FOREIGN KEY (space_id) REFERENCES spaces(space_id)
+);
---
+++ spec/seeds/makers_bnb_new_seed.sql
@@ -0,0 +1,106 @@
+TRUNCATE TABLE users RESTART IDENTITY CASCADE;
+TRUNCATE TABLE spaces RESTART IDENTITY CASCADE;
+TRUNCATE TABLE reservations RESTART IDENTITY CASCADE;
+
+INSERT INTO users (first_name, last_name, email, password) VALUES
+('John', 'Parker', 'test2@example.com', crypt('password2', gen_salt('bf', 8))),
+('Anna', 'Jones', 'ajones@example.com', crypt('password3', gen_salt('bf', 8))),
+('Liz', 'Smith', 'lizs@example.com', crypt('password4', gen_salt('bf', 8))),
+('Michael', 'Tyson', 'mt1245@example.com', crypt('password5', gen_salt('bf', 8)));
+
+WITH john AS (
+   SELECT user_id FROM users WHERE first_name = 'John')
+
+ INSERT INTO spaces (title, description, address, price_per_night, available_from, available_to, host_id)
+   SELECT 'beach view', 'a modern house on the beach', 'Camber S1 00J', 100, '2022/07/19', '2022/11/19', user_id
+   FROM john;
+
+WITH anna AS (
+   SELECT user_id FROM users WHERE first_name = 'Anna')
+
+ INSERT INTO spaces (title, description, address, price_per_night, available_from, available_to, host_id)
+   SELECT 'mountain view', 'a modern house in the mountains', 'Alpes 12345', 150, '2022/06/19', '2022/09/19', user_id
+   FROM anna;
+
+WITH anna AS (
+  SELECT user_id FROM users WHERE first_name = 'Anna')
+
+  INSERT INTO spaces (title, description, address, price_per_night, available_from, available_to, host_id)
+    SELECT 'review view', 'a house in the banks of the Rhodes', 'Crillon 12345', 80,'2022/03/19', '2023/09/19', user_id
+    FROM anna; 
+
+WITH michael AS (
+  SELECT user_id FROM users WHERE first_name = 'Michael')
+
+  INSERT INTO spaces (title, description, address, price_per_night, available_from, available_to, host_id)
+    SELECT 'city getaway', 'a bright private room in Central London', 'London SW1 0UJ', 300, '2022/07/19', '2023/07/17', user_id
+    FROM michael;
+
+
+WITH anna AS (
+   SELECT user_id AS host_id FROM users WHERE first_name = 'Anna'),
+
+  annas_space AS (
+    SELECT space_id FROM spaces WHERE address = 'Alpes 12345'),
+
+  john AS (
+    SELECT user_id AS guest_id FROM users WHERE first_name = 'John')
+
+    INSERT INTO reservations (start_date, end_date, number_night, confirmed, host_id, guest_id, space_id)
+      SELECT
+      '2022-07-22',
+      '2022-07-31',
+      9,
+      true,
+      host_id , guest_id, space_id FROM anna, john, annas_space;
+
+WITH john AS (
+   SELECT user_id AS host_id FROM users WHERE first_name = 'John'),
+
+  jones_space AS (
+    SELECT space_id FROM spaces WHERE address = 'Camber S1 00J'),
+
+  anna AS (
+    SELECT user_id AS guest_id FROM users WHERE first_name = 'Anna')
+
+    INSERT INTO reservations (start_date, end_date, number_night, confirmed, host_id, guest_id, space_id)
+      SELECT
+      '2022-07-19',
+      '2022-08-31',
+      43,
+      true,
+      host_id , guest_id, space_id FROM john, anna, jones_space;
+
+WITH anna AS (
+   SELECT user_id AS host_id FROM users WHERE first_name = 'Anna'),
+
+  annas_space AS (
+    SELECT space_id FROM spaces WHERE address = 'Alpes 12345'),
+
+  liz AS (
+    SELECT user_id AS guest_id FROM users WHERE first_name = 'Liz')
+
+    INSERT INTO reservations (start_date, end_date, number_night, confirmed, host_id, guest_id, space_id)
+      SELECT
+      '2022-09-01',
+      '2022-09-07',
+      6,
+      false,
+      host_id, guest_id, space_id FROM anna, liz, annas_space;
+
+WITH michael AS (
+   SELECT user_id AS host_id FROM users WHERE first_name = 'Michael'),
+
+  michaels_space AS (
+    SELECT space_id FROM spaces WHERE address = 'London SW1 0UJ'),
+
+  liz AS (
+    SELECT user_id AS guest_id FROM users WHERE first_name = 'Liz')
+
+    INSERT INTO reservations (start_date, end_date, number_night, confirmed, host_id, guest_id, space_id)
+      SELECT
+      '2022-12-25',
+      '2023-01-05',
+      11,
+      true,
+      host_id , guest_id, space_id FROM michael, liz, michaels_space; 
