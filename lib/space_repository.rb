087b1b35dbc179f5646c76a4aade6d745d93483d
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-19 18:58:13 +0100

    commented out delete method and fixed map_PG_to_objects bug

--- lib/space_repository.rb
+++ lib/space_repository.rb
@@ -16,18 +16,17 @@ class SpaceRepository
   end
 
   def create(space)
-    sql = "INSERT INTO spaces (title, description, address, available_from, available_to, host_id) VALUES ($1, $2, $3, $4, $5, $6)"
-    result_set = DatabaseConnection.exec_params(sql, [space.title, space.description, space.address, space.available_from, space.available_to, space.host_id])
+    sql = "INSERT INTO spaces (title, description, address, price_per_night, available_from, available_to, host_id) VALUES ($1, $2, $3, $4, $5, $6, $7)"
+    result_set = DatabaseConnection.exec_params(sql, [space.title, space.description, space.address, space.price_per_night, space.available_from, space.available_to, space.host_id])
 
     # side effect: returns PG object
   end
 
-  def delete(space)
-    sql = "DELETE FROM spaces WHERE space_id = $1;"
-    result_set = DatabaseConnection.exec_params(sql, [space.space_id])
-
-    # side effect: returns PG object
-  end
+  # def delete(space)
+  #   sql = "DELETE FROM spaces WHERE space_id = $1;"
+  #   result_set = DatabaseConnection.exec_params(sql, [space.space_id])
+  #   # side effect: returns PG object
+  # end
 
   def update_availability(space)
     sql = "UPDATE spaces SET available_from = $1, available_to = $2 WHERE space_id = $3;"
@@ -57,13 +56,14 @@ class SpaceRepository
 
     result_set.each do |record|
       space = Space.new
-
-      space.title = record[:title]
-      space.description = record[:description]
-      space.address = record[:address]
-      space.available_from = record[:available_from]
-      space.available_to = record[:available_to]
-      space.host_id = record[:host_id]
+      space.space_id = record["space_id"]
+      space.title = record["title"]
+      space.description = record["description"]
+      space.address = record["address"]
+      space.price_per_night = record["price_per_night"]
+      space.available_from = record["available_from"]
+      space.available_to = record["available_to"]
+      space.host_id = record["host_id"]
 
       spaces << space
     end
