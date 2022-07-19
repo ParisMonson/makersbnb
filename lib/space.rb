0de97dc79b8750a54e639a1b1756f4cd1239b1e5
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-19 17:11:15 +0100

    added price_per_night attr

--- lib/space.rb
+++ lib/space.rb
@@ -1,3 +1,3 @@
 class Space
-  attr_accessor :space_id, :title, :description, :address, :available_from, :available_to, :host_id
+  attr_accessor :space_id, :title, :description, :address, :price_per_night, :available_from, :available_to, :host_id
 end
