15249f42a6f130dfaf3612005d36b317b7e09341
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-20 11:10:48 +0100

    implemented part of get / route

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
-require 'sinatra/base'
-require 'sinatra/reloader'
+require "sinatra/base"
+require "sinatra/reloader"
+require_relative "lib/database_connection"
+require_relative "lib/space_repository"
 
-# DatabaseConnection.connect
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
@@ -17,3 +19,10 @@ class Application < Sinatra::Base
   post "/signup" do
     # Need UserRepository to check if email is in use
   end
+
+  get "/" do
+    @spaces = SpaceRepository.new.all
+
+    erb :index
+  end
+end
