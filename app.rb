dc8868b782d86439e5dc43aad54f2c7b8fdbb13c
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-07-22 09:06:18 +0100

    added input validation for sign up form + linked the reservation form on index.erb  to post/request/?

--- app.rb
+++ app.rb
@@ -28,18 +28,23 @@ class Application < Sinatra::Base
   end
 
   post "/signup" do
-    #I think we need to check that the email is uniq at this stage <<<<<<
-    repo_users = UserRepository.new
-    new_user = User.new
-    new_user.first_name = params[:first_name]
-    new_user.last_name = params[:last_name]
-    new_user.email = params[:email]
-    new_user.password = params[:password]
-    repo_users.create_user(new_user)
-    @user = repo_users.find_user(params[:email])
-    session[:user_id] = @user.user_id
-    redirect "/signup/success"
-    # to add a conditional - if the input data is correct
+    @error = nil
+    signup_input_validation
+    if @error != nil
+      @error
+      return erb(:signup)
+    else
+      repo_users = UserRepository.new
+      new_user = User.new
+      new_user.first_name = params[:first_name]
+      new_user.last_name = params[:last_name]
+      new_user.email = params[:email]
+      new_user.password = params[:password]
+      repo_users.create_user(new_user)
+      @user = repo_users.find_user(params[:email])
+      session[:user_id] = @user.user_id
+      redirect "/signup/success"
+    end
   end
   
   get "/signup/success" do
@@ -135,6 +140,23 @@ class Application < Sinatra::Base
     end 
   end
 
+  
+  get "/:space_id" do
+    space_id = params[:space_id]
+    @space = SpaceRepository.new.find_by_space_id(space_id)[0]
+    @host_name = UserRepository.new.find_by_id(@space.host_id).first_name
+    
+    erb :individual_space
+  end
+  
+  post "/requests/:reservation_id" do
+    repo = ReservationRepository.new
+    repo.confirm_reservation(params[:reservation_id])
+    redirect "/requests"
+  end
+
+  private
+
   def input_validation
     if (params[:title].length == 0 || params[:address].length == 0 || params[:price_per_night].length == 0 || params[:available_from].length == 0 || params[:available_to.length == 0])
       @error = "missing information error"
@@ -149,18 +171,17 @@ class Application < Sinatra::Base
     end
     return @error
   end
-  
-   get "/:space_id" do
-    space_id = params[:space_id]
-    @space = SpaceRepository.new.find_by_space_id(space_id)[0]
-    @host_name = UserRepository.new.find_by_id(@space.host_id).first_name
 
-    erb :individual_space
-   end
-
-  post "/requests/:reservation_id" do
-    repo = ReservationRepository.new
-    repo.confirm_reservation(params[:reservation_id])
-    redirect "/requests"
+  def signup_input_validation
+    repo_users = UserRepository.new
+    if ((params[:first_name].length == 0) || (params[:last_name].length == 0) || (params[:email].length == 0) || (params[:password].length == 0))
+      @error = "input_missing"
+    elsif (params[:first_name].match?(/[^a-z\s-]{2,30}/i)|| params[:last_name].match?(/[^a-z\s-]{2,30}/i))
+      @error = "invalid_name"
+    elsif !repo_users.find_user(params[:email]).nil?
+      @error = "existing_email"
+    end
+    return @error
   end
+  
 end
